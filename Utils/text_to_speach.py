fa91c2dcd901c16ee848993d12f45c089426ab8f
Klas Holmgren <klas0holmgren@gmail.com>
2023-05-29 21:50:23 +0200

    Text to audio

--- Utils/text_to_speach.py
+++ Utils/text_to_speach.py
@@ -1,12 +1,40 @@
 import re
 import ffmpeg
-
+import pyttsx3
+import time
 
 def text_to_audio(subtitles):
     """
-    takes a subtitle string and uses text to speach api to construct an audio file with speach at the specific time points and scielence in between
+    Takes a subtitle string and uses text-to-speech API to construct an audio file with speech at the specific time points and silence in between.
     """
-    pass
+    # Initialize the TTS engine
+    engine = pyttsx3.init()
+
+    # Set the audio file format (e.g., "wav", "mp3")
+    audio_format = "wav"
+
+    # Split the subtitle string into individual time points and speech texts
+    subtitle_parts = subtitles.split("\n")
+    time_points = [part.split(":")[0] for part in subtitle_parts]
+    speech_texts = [part.split(":")[1] for part in subtitle_parts]
+
+    # Iterate over the subtitle parts and convert them to audio
+    for i in range(len(subtitle_parts)):
+        # Extract the time point and speech text
+        time_point = time_points[i]
+        speech_text = speech_texts[i]
+
+        # Convert the speech text to audio
+        engine.save_to_file(speech_text, f"audio_{i}.{audio_format}")
+
+        # Wait until the specified time point to continue
+        current_time = time.strftime("%H:%M", time.gmtime())
+        while current_time < time_point:
+            current_time = time.strftime("%H:%M", time.gmtime())
+            time.sleep(1)
+
+    # Run the TTS engine's event loop and wait for all the audio files to be generated
+    engine.runAndWait()
 
 
 
